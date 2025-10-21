bc2a051bf9baea48a07d975100ec154b7b78883a
Álvaro <alvaro.silva@udc.es>
2025-10-21 19:15:03 +0200

    Reconocimiento de expresiones multi-línea

--- main.ml
+++ main.ml
@@ -6,13 +6,38 @@ open Lambda;;
 open Parser;;
 open Lexer;;
 
+
+let read_multiline () =
+  let rec aux acc =
+    let line = read_line () in
+    if String.contains line ';' && String.length line > 1 then
+      let has_double = ref false in
+      let pos = ref 0 in
+      for i = 0 to String.length line - 2 do
+        if line.[i] = ';' && line.[i+1] = ';' then begin
+          has_double := true;
+          pos := i
+        end
+      done;
+      if !has_double then
+        let line_without = String.sub line 0 !pos in
+        String.concat " " (List.rev (line_without :: acc))
+      else
+        aux (line :: acc)
+    else
+      aux (line :: acc)
+  in
+  aux []
+;;
+
+
 let top_level_loop () =
   print_endline "Evaluator of lambda expressions...";
   let rec loop ctx =
     print_string ">> ";
     flush stdout;
     try
-      let tm = s token (from_string (read_line ())) in
+      let tm = s token (from_string (read_multiline())) in
       let tyTm = typeof ctx tm in
       print_endline (string_of_term (eval tm) ^ " : " ^ string_of_ty tyTm);
       loop ctx
